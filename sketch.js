2965dabb82cf586b98ecfcbc0cd0473a1f0342c7
akakkar2006 <akakkar2006@gmail.com>
2020-08-12 12:31:00 -0400

    Added contraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +37,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    constrainedLog= new Log(200,200,100,PI/20);
+
+   
+    chain = new Chain(bird.body,constrainedLog.body);
 
 }
 
@@ -61,4 +67,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+
+    chain.display();
 }
