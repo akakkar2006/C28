a82bee779c69850edb300e017445b4352b150bb7
akakkar2006 <akakkar2006@gmail.com>
2020-08-14 12:32:15 -0400

    added slingshot

--- sketch.js
+++ sketch.js
@@ -37,19 +37,19 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-    constrainedLog= new Log(200,200,100,PI/20);
+   
 
    
-    chain = new Chain(bird.body,constrainedLog.body);
+    chain = new Slingshot(bird.body,{x:200,y:100});
 
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -67,7 +67,15 @@ function draw(){
 
     bird.display();
     platform.display();
-    constrainedLog.display();
+   
 
     chain.display();
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+    chain.fly();
 }
